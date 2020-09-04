73dc45b7966a319ca9abd956e6f2e5082cf91b76
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-09-04 00:48:53 -0400

    upload video

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -25,12 +25,12 @@ function frame() {
 PlayVid = function() {
     //get the Video div and play new video on button click
     var videoPanel = document.getElementById('vid');
-    videoPanel.innerHTML = '<iframe width="900" height="500" src="//www.youtube.com/embed/ldT8qxWh07Y?autoplay=1&start=17" frameborder="0" allowfullscreen></iframe>';
+    videoPanel.innerHTML = '<iframe width="900" height="500" src="https://youtu.be/rNO_FSW5XmA" frameborder="0" allowfullscreen></iframe>';
     setTimeout(resetVid, 8500);
 }
 
 PrevVid = function(){
     var videoPanel = document.getElementById('vid');
-    videoPanel.innerHTML = '<iframe width="900" height="500" src="/videos/introduction.mp4" frameborder="0" allowfullscreen></iframe>';
+    videoPanel.innerHTML = '<iframe width="900" height="500" src="" frameborder="0" allowfullscreen></iframe>';
     setTimeout(resetVid, 8500);
 }
